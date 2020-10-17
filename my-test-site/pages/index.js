7bb52b62f9f90fe3a937aa790db680c7a413e5fe
ItsTeeJay <itsteejay29@gmail.com>
2020-10-17 12:54:15 -0700

    Basic material-ui setup

--- my-test-site/pages/index.js
+++ my-test-site/pages/index.js
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Button from '@material-ui/core/Button';
 
 export default function Home() {
   return (
     <div>
-    Hello World
-    hello world
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
     </div>
   )
 }
