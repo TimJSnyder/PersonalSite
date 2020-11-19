d0c43f1925e45435dd3f7e6890b0559a4f787382
ItsTeeJay <itsteejay29@gmail.com>
2020-11-19 00:54:32 -0800

    added backend function to the mail button

--- Components/Mail.js
+++ Components/Mail.js
@@ -9,18 +9,53 @@ import {
   useTheme,
 } from '@material-ui/core';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
-import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+
+const BASE_URL = 'https://v9fq8p6ad9.execute-api.us-west-2.amazonaws.com/';
 
 export default function Mail() {
   const theme = useTheme();
   const iconStyle = { color: theme.palette.secondary.contrastText };
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+
   const handleClickOpen = () => {
     setOpen(true);
   };
 
   const handleClose = () => {
     setOpen(false);
+  };
+  const resetForm = () => {
+    setEmail('');
+    setMessage('');
+    setName('');
+    setSubject('');
+  };
+
+  const handleSend = async () => {
+    if (!name || !email || !subject || !message) {
+      window.alert('Please fill out all fields');
+      return;
+    }
+    try {
+      resetForm();
+      const data = {
+        name, email, subject, message,
+      };
+      const headers = { 'content-type': 'application/json' };
+
+      const response = await axios.post(BASE_URL, data, { headers });
+      console.log(response);
+      setOpen(false);
+    } catch (error) {
+      console.error(error);
+      setOpen(false);
+    }
   };
 
   return (
@@ -32,23 +67,31 @@ export default function Mail() {
         <DialogTitle>Send me an email</DialogTitle>
         <DialogContent>
           <TextField
+            value={name}
+            onChange={({ target }) => setName(target.value)}
             autoFocus
             margin="dense"
             label="Your Name"
           />
           <TextField
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
             autoFocus
             margin="dense"
             label="Your Email"
             fullWidth
           />
           <TextField
+            value={subject}
+            onChange={({ target }) => setSubject(target.value)}
             autoFocus
             margin="dense"
             label="Subject"
             fullWidth
           />
           <TextField
+            value={message}
+            onChange={({ target }) => setMessage(target.value)}
             autoFocus
             margin="dense"
             label="Message"
@@ -61,7 +104,7 @@ export default function Mail() {
           <Button onClick={handleClose} color="primary">
             Cancel
           </Button>
-          <Button onClick={handleClose} color="primary">
+          <Button onClick={handleSend} color="primary">
             Send
           </Button>
         </DialogActions>
