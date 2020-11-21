1fd9e5cbc4b3310e4833e620b7821563ff1c0a9f
ItsTeeJay <itsteejay29@gmail.com>
2020-11-20 20:46:22 -0800

    minor css change to appbar

--- Components/About.js
+++ Components/About.js
@@ -5,7 +5,15 @@ export default function About() {
     <div id="about">
       <Container className="aboutContainer">
         <h1 className="textCenter">About Me</h1>
-        <p className="aboutParagraph">"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+        <p className="aboutParagraph">
+          Hello! My name is Tim Snyder. I am currently a student at Bellevue College,
+          and I am pursuing a bachelor’s degree in computer science. I enjoy learning
+          about software development and study languages and frameworks when I am not in school.
+          Outside of school, I work at a nonprofit that provides childcare to in-need families.
+          Right now, I am learning a lot about React and MaterialUI that I use for web projects
+          such as this site but I also am learning java and python as well. Feel free to contact
+          me anytime :)
+        </p>
       </Container>
     </div>
   );
